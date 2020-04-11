3f4190fafbc6bf5284745387ce4aa4849d24d0a2
Jehan Lucas <lucasarago.df@gmail.com>
2020-04-10 21:28:58 -0300

    Criação das posicoes

--- src/Principal.java
+++ src/Principal.java
@@ -1,10 +1,13 @@
+import tabuleirogame.Posicao;
 
 public class Principal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Hellol World");
+		Posicao p1 =  new Posicao(3,5);
+		
+		System.out.println(p1);
 
 	}
 
